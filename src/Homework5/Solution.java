a4d80912c60c077b670b7ad9e4358f5b66146827
OzerBey <ysnozer.zer@gmail.com>
2021-06-14 16:35:38 +0300

    edited

--- src/Homework5/Solution.java
+++ src/Homework5/Solution.java
@@ -2,19 +2,6 @@ package Homework5;
 
 import java.util.Scanner;
 
-/*
- * Alan Formülü : π * r * r;
-
-Çevre Formülü : 2 * π * r;
-
-Ödev
-Yarıçapı r, merkez açısısının ölçüsü 𝛼 olan daire diliminin alanı bulan programı yazınız.
-
-𝜋 sayısını = 3.14 alınız.
-
-Formül : (𝜋 * (r*r) * 𝛼) / 360
-
-*/
 public class Solution {
 
 	public static void main(String[] args) {
