0fb8c164aa7fe7c5718e22fabde0476f3d538ce5
OzerBey <ysnozer.zer@gmail.com>
2021-06-17 16:18:01 +0300

    homework23(Program to find harmonic series)

---
+++ src/Homework23/Readme.md
@@ -0,0 +1,8 @@
+
+
+> Harmonik Seri Bulan Program
+
+Java ile girilen sayının harmonik serisini bulan program yazacağız.
+
+Harmonik Seri Formülü :
+![image](https://user-images.githubusercontent.com/49093196/122403804-521b7d80-cf87-11eb-98f5-926266854237.png)
---
+++ src/Homework23/Solution.java
@@ -0,0 +1,19 @@
+package Homework23;
+
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("N sayısını giriniz :");
+		double n = input.nextInt();
+		double result = 0;
+		while (n > 0) {
+			result += (1 / n);
+			n--;
+		}
+
+		System.out.println(result);
+	}
+
+}
