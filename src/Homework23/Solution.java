498ed6431db3292d15f41d35dade3382b581bc07
OzerBey <ysnozer.zer@gmail.com>
2021-06-17 16:42:59 +0300

    program that calculate harmonic numbers edited

--- src/Homework23/Solution.java
+++ src/Homework23/Solution.java
@@ -4,15 +4,24 @@ import java.util.Scanner;
 
 public class Solution {
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		System.out.println("N sayısını giriniz :");
-		double n = input.nextInt();
-		double result = 0;
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter N number ");
+		double n = scan.nextInt();
+		/*double result = 0;
 		while (n > 0) {
 			result += (1 / n);
 			n--;
 		}
+		System.out.println(result);
+	*/
+		calculateHarmonicNumbers(n);
+	}
 
+	static void calculateHarmonicNumbers(double n) {
+		double result = 0;
+		for (double i = n; i > 0; i--) {
+			result += (1 / i);
+		}
 		System.out.println(result);
 	}
 
